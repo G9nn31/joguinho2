d1dadaa2843141fd969ee859d5378f333dda6ffc
G9nn31 <145568087+G9nn31@users.noreply.github.com>
2024-08-23 17:01:01 -0300

    projeto salvo1

---
+++ oficina2/Assets/sprits/player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += moviment * Time.deltaTime *Speed;
+
+    }
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+    }
+}
