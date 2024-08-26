e78ca274656e7a6d6c460840d40df30bd4e01aba
G9nn31 <145568087+G9nn31@users.noreply.github.com>
2024-08-26 08:30:15 -0300

    projeto salvo 2

--- oficina2/Assets/sprits/player.cs
+++ oficina2/Assets/sprits/player.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class player : MonoBehaviour
 {
+
+
+
     public float Speed;
+    public float jumpForce;
+    private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        jump();
     }
 
     void Move()
     {
-        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
-        transform.position += moviment * Time.deltaTime *Speed;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += movement * Time.deltaTime *Speed;
 
     }
-    void Jump()
+    void jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("jump"))
+        {
+            rig.AddForce(new Vector2 (0f,jumpForce),ForceMode2D.Impulse);
+        }
     }
 }
