19fe513b848ede7c3440ae40c149fc0e7f53b00c
G9nn31 <giovanna.clementino@escolar.ifrn.edu.br>
2024-09-17 17:36:39 -0300

    novo

--- oficina2/Assets/Scripts/EfeitosDigitador.cs
+++ oficina2/Assets/Scripts/EfeitosDigitador.cs
@@ -10,22 +10,48 @@ public class NewBehaviourScript : MonoBehaviour
     private TextMeshProUGUI componenteTexto;
     private AudioSource _audioSource;
     private String mensagemOriginal;
-
     public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
 
 
 
     private void Awake()
     {
-
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
     }
 
     private void OnEnable()
     {
-
+        ImprimirMensagem(mensagemOriginal);
     }
     private void OnDisable()
     {
-        
+        componenteTexto.text = mensagemOriginal;
+        stopAllCoroutines();
+    }
+    public  void ImprimirMensagem(string mensagem)
+    {
+        if(gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StarCoroutine(mensagem);
+        }
+    }
+    IEnumerator LetraPorLetra( String mensagem)
+    {
+        string msg = "";
+        foreach(var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras );
+        }
+        imprimindo = false;
+        stopAllCoroutines();
     }
 }
